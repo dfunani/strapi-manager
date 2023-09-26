1ace89a111c530cc793cd6cbd1d05918f7c835c8
dfunani <dfunani@live.co.za>
2023-09-27 00:21:38 +0200

    added updater

---
+++ strapi_updater_server/app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const {
+  setCollectionsData,
+  getItemIds,
+  getPermissionIds,
+  strapiUpdate,
+  setMicroAppState,
+  getWhitelistArray,
+  getProfileId,
+} = require("./utils");
+require("dotenv").config();
+const path = require("path");
+const axios = require("axios");
+
+const PORT = process.env.PORT ?? 10000
+
+const app = express();
+
+app.use(express.json());
+
+let collectionsData = {};
+setCollectionsData().then((data) => {
+  collectionsData = data;
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/app.html"));
+});
+
+app.post("/update", async (req, res) => {
+try {
+  //   const dummy = await axios.get(
+//     process.env.STRAPI_URL + "/api/micro-apps/1040/?populate=*"
+//   );
+//   const microapp = dummy.data.data.attributes;
+//   const microappId = dummy.data.data.id;
+const microapp = req.body.attributes
+const microappId = req.body.id
+delete microapp.icon; //if icon is not removed, strapi will remove existing icon image
+
+microapp.languages_ = getItemIds(
+microapp.languages[0]?.name,
+collectionsData.languages.data
+);
+microapp.categories_ = getItemIds(
+microapp.category,
+collectionsData.categories.data
+);
+microapp.permissions_ = getPermissionIds(
+microapp,
+collectionsData.permissions.data
+);
+microapp.countries_ = getItemIds(
+microapp.countries[0]?.name,
+collectionsData.countries.data
+);
+microapp.whitelist_ = getWhitelistArray(microapp.whitelist[0]?.links)
+
+microapp.lifecycle = await setMicroAppState(microapp, microappId);
+
+// const profileId = await getProfileId(microapp.user_id)
+
+const data = {
+languages_: [...microapp.languages_],
+categories_: [...microapp.categories_],
+permissions_: [...microapp.permissions_],
+countries_: [...microapp.countries_],
+lifecycle: [...microapp.lifecycle],
+whitelist_: [...microapp.whitelist_],
+// user: Number(microapp.user_id),
+// profile_: profileId
+};
+
+strapiUpdate(microappId, data)
+  .then((data) => res.status(200).json({ ...data }))
+  .catch((err) => res.status(400).json(err));
+
+} catch (error) {
+  return res.status(500).json({error})
+}
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
---
+++ strapi_updater_server/utils.js
@@ -0,0 +1,194 @@
+const axios = require("axios");
+require("dotenv").config();
+const fs = require("fs");
+
+const baseUrl = process.env.STRAPI_URL ?? "http://localhost:1337";
+
+const states = [
+  "development",
+  "requested to publish",
+  "awaiting documents",
+  "vetting",
+  "failed",
+  "published",
+  "suspended",
+  "deleted",
+  "unpublished",
+];
+
+exports.setCollectionsData = async function () {
+  const collectionData = {};
+  collectionData.categories = await fetchQuery("api/categories");
+  collectionData.languages = await fetchQuery("api/language-items");
+  collectionData.permissions = await fetchQuery("api/permission-items");
+  collectionData.countries = await fetchQuery("api/countries");
+  return collectionData;
+};
+
+const fetchQuery = async function (path) {
+  let url = `${baseUrl}/${path}`;
+  return await axios
+    .get(url)
+    .then((data) => data.data)
+    .catch((err) => err.message);
+};
+/**
+ * returns collection items IDs.
+ * returned value is an array of language IDs that match selected languages
+ * @param {string} selectedItems JSON Array
+ * @param {[{id: number, attributes: {name: string}}]} collectionData
+ * @returns {[number]}
+ */
+exports.getItemIds = function (selectedItems, collectionData) {
+  if (typeof selectedItems !== "string") return [];
+  if (!selectedItems.length) return [];
+  if (!Array.isArray(collectionData)) return [];
+  if (!collectionData.length) return [];
+  
+  const list = collectionData.filter((item) => {
+    if (
+      selectedItems
+        .toLocaleLowerCase()
+        .includes(item.attributes.name.toLocaleLowerCase())
+    )
+      return item.id;
+  });
+  return list.map((item) => item.id);
+};
+/**
+ * return selected permission ids
+ * returned value is an array of permission IDs that match selected permissions
+ * @param {{}} microappInfo microapp data
+ * @param {[{id: number, name: string}]} permissionData
+ * @returns {[number]}
+ */
+exports.getPermissionIds = function (microappInfo, permissionData) {
+  if (typeof microappInfo !== "object") return [];
+  if (!Array.isArray(permissionData)) return [];
+  if (!permissionData.length) return [];
+
+  const list = [];
+  permissionData.forEach((item) => {
+    if (microappInfo[item.attributes.name] === true) {
+      list.push(item.id);
+    }
+  });
+  return list;
+};
+/**
+ * coverts json array to array of objects with url property
+ * @param {JSON} jsonWhitelist JSON string array
+ * @returns {[{url: string}]}
+ */
+exports.getWhitelistArray = function (jsonWhitelist) {
+  if (!isJSON(jsonWhitelist)) {
+    if (typeof jsonWhitelist == "string") return [{ url: jsonWhitelist }];
+    else return [];
+  }
+  const strArr = JSON.parse(jsonWhitelist);
+  if (!Array.isArray(strArr)) return [];
+  return strArr.map((item) => ({ url: item }));
+};
+exports.strapiUpdate = async function (id, microapp) {
+  if (!id) return;
+  if (!microapp) return;
+
+  return await axios
+    .put(`${baseUrl}/api/micro-apps/${id}`, { data: microapp })
+    .then((data) => {
+      return Promise.resolve(data.data);
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+};
+
+exports.setMicroAppState = async (microapp, id) => {
+  const requests = await getRequests(id);
+  return states
+    .map((state) => {
+      switch (state) {
+        case "development":
+          return returnTemplate(
+            state,
+            "app in development, ready for testing",
+            microapp?.createdAt || null
+          );
+        case "published":
+          if (!microapp.published) return null;
+          return returnTemplate(state, "", microapp?.live_date || null);
+        case "suspended":
+          if (!microapp.suspended) return null;
+          return returnTemplate(
+            state,
+            microapp?.suspension_reason || "",
+            microapp?.suspension_date || null
+          );
+        case "deleted":
+          if (!microapp.deleted) return null;
+          return returnTemplate(state, "", microapp?.updatedAt || null);
+        case "requested to publish":
+          if (!requests.hasOwnProperty("attributes")) return null;
+          return returnTemplate(
+            state,
+            "",
+            microapp?.rtp_date || requests.attributes.updatedAt
+          );
+        default:
+          return null;
+      }
+    })
+    .filter((state) => state);
+};
+
+function returnTemplate(state, comment, date) {
+  return {
+    comment: comment,
+    status: state,
+    date: date,
+  };
+}
+
+async function getRequests(id) {
+  return await axios
+    .get(
+      baseUrl +
+        "/api/publication-requests?populate=*&filters[microapp_id][$eq]=" +
+        id
+    )
+    .then((result) => result.data?.data[0] ?? {})
+    .catch((err) => err);
+}
+
+exports.getProfileId = async function (userId) {
+  return await axios
+    .get(
+      baseUrl + `/api/profiles?filters[user_id][$eq]=${userId}`
+    )
+    .then((res) => {
+      if (res.data.data.length) return res.data.data[0].id;
+      return null;
+    })
+    .catch((err) => err);
+};
+
+exports.logError = function (appId, error) {
+  const errorJson = JSON.stringify(error, null, 2);
+  fs.writeFile("error.txt", errorJson, (err) => {
+    if (err) console.error("Error writing to log file:", err);
+  });
+};
+
+/**
+ * returns true if value is JSON string else returns false
+ * @param {*} value
+ * @returns {Boolean}
+ */
+function isJSON(value) {
+  try {
+    JSON.parse(value);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
